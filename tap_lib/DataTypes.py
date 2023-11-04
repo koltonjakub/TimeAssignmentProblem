03207305357219db36b9a7e15ea815ef751cb007
koltonjakub <102661671+koltonjakub@users.noreply.github.com>
2023-11-04 23:44:56 +0100

    Scope moved to DataTypes, is now pydantic.BaseModel type

--- tap_lib/DataTypes.py
+++ tap_lib/DataTypes.py
@@ -1,20 +1,49 @@
 """This file contains all data types used in project"""
 
+from numpy import max, int32, int64, float32, float64
+from pydantic import BaseModel, Field, BaseConfig, conint
+from typing import Union, Dict, List, Any, Annotated
+from collections import defaultdict
+from enum import Enum
 
-import numpy as np
-import pydantic as pdt
-import typing as typ
-import collections as coll
-import enum
+
+NonNegativeInt = Union[int, int32, int64]
+RealNumber = Union[float, float32, float64, int, int32, int64]
+PositiveNumber = Union[float, float32, float64, int, int32, int64]
 
 
-class Resource(pdt.BaseModel):
+class Scope(BaseModel):
+    """BaseModel that stores all the runtime data of simulation"""
+    class Config(BaseConfig):
+        arbitrary_types_allowed = True  # Allows for validation of numpy numeric types
+        validate_assignment = True  # Allows the model to validate data every time
+        smart_union = True  # Prevents unnecessary rounding to int
+
+    iteration: Annotated[List[NonNegativeInt], Field(ge=0)] = []
+    temperature: Annotated[List[PositiveNumber], Field(ge=0)] = []
+    delta_energy: Annotated[List[RealNumber], Field()] = []
+    probability_of_transition: Annotated[List[RealNumber], Field(ge=0, le=1)] = []
+    cost_function: Annotated[List[RealNumber], Field()] = []
+    visited_solution: Annotated[List[Any], Any] = []
+
+
+class ScopeParams(Enum):
+    """Enum type of all data available within Scope type"""
+    iteration: str = 'iteration'
+    temperature: str = 'temperature'
+    delta_energy: str = "delta_energy"
+    probability_of_transition: str = "prob_of_trans"
+    cost_function: str = 'cost'
+    visited_solution: str = 'visited_solution'
+
+
+class Resource(BaseModel):
     """Informal interface for any type of resource_type that may be used in a factory"""
 
-    hourly_cost: typ.Union[int, float]
-    hourly_gain: typ.Union[int, float]
+    hourly_cost: Union[int, float]
+    hourly_gain: Union[int, float]
 
-    id: pdt.conint(ge=0)
+    id: conint(ge=0)
 
 
 class Employee(Resource):
@@ -34,7 +63,7 @@ class Machine(Resource):
     # TODO add the rest of necessary fields
 
 
-class AvailableResources(enum.Enum):
+class AvailableResources(Enum):
     """Enum type, lists all available types of resources"""
     EMPLOYEE = Employee
     MACHINE = Machine
@@ -44,7 +73,7 @@ class ResourceManager:
     """Class that manages all types of resources within the project"""
 
     def __init__(self):
-        self.used_ids: typ.Dict[AvailableResources: int] = coll.defaultdict(None)
+        self.used_ids: Dict[AvailableResources, int] = defaultdict(None)
 
     def create_resource(self, data, resource_type: AvailableResources) -> Resource:
         data['id']: int = self.update_resource_ids(resource_type=resource_type)
@@ -57,12 +86,12 @@ class ResourceManager:
     def update_resource_ids(self, resource_type: AvailableResources) -> int:
         if resource_type not in self.used_ids:  # assign first id of provided Resource type
             new_id = 0
-            self.used_ids[resource_type] = [new_id]
+            self.used_ids[resource_type]: List[int] = [new_id]
 
             return new_id
 
         else:  # increment id of provided type
-            new_id = np.max(self.used_ids[resource_type]) + 1
+            new_id = max(self.used_ids[resource_type]) + 1
             self.used_ids[resource_type].append(new_id)
 
             return new_id
@@ -79,8 +108,8 @@ class ResourceManager:
 def resources_creation_example():
     manager = ResourceManager()  # call the manager, should be used as singleton
 
-    employees = [manager.create_resource(data={'hourly_cost': 1, 'hourly_gain': 1,    # pass data
-                                               'name': 'John', 'surname': 'Smith'},   # pass data
+    employees = [manager.create_resource(data={'hourly_cost': 1, 'hourly_gain': 1,  # pass data
+                                               'name': 'John', 'surname': 'Smith'},  # pass data
                                          resource_type=AvailableResources.EMPLOYEE),  # pass type
                  manager.create_resource(data={'hourly_cost': 1, 'hourly_gain': 1,
                                                'name': 'Adam', 'surname': 'Nash'},
@@ -108,14 +137,32 @@ def validation_errors_example():
     manager = ResourceManager()
 
     # proper definition od machine instance
-    machine = manager.create_resource(data={'hourly_cost': 1, 'hourly_gain': 1, 'inventory_nr': 123},
-                                      resource_type=AvailableResources.MACHINE)
+    _ = manager.create_resource(data={'hourly_cost': 1, 'hourly_gain': 1, 'inventory_nr': 123},
+                                resource_type=AvailableResources.MACHINE)
 
     # both non-optional fields not provided
-    employee = manager.create_resource(data={'name': 'John', 'surname': 'Smith'},
-                                       resource_type=AvailableResources.EMPLOYEE)
+    _ = manager.create_resource(data={'name': 'John', 'surname': 'Smith'},
+                                resource_type=AvailableResources.EMPLOYEE)
+
+
+def scope_usage_example():
+    scp = Scope()
+    scp.iteration = [1, 0, 2.0]  # Type coercion is always ON, probably due to
+    scp.iteration += [1]  # The only proper way to append to list, .append() method omits the pydantic validation
+
+    scp.probability_of_transition = [1.2]
+    scp.probability_of_transition += [1.2]
+    scp.delta_energy = [-1]
+    scp.delta_energy += [-1]
+
+    scp.visited_solution += [(1, 1)]
+
+    print('Scope:')
+    print(scp)
+    print()
 
 
 if __name__ == "__main__":
+    scope_usage_example()
     resources_creation_example()
     validation_errors_example()
--- tap_lib/SimulatedAnnealing.py
+++ tap_lib/SimulatedAnnealing.py
@@ -1,23 +1,17 @@
-"""Algorithm that executes simulated annealing method."""
+"""Algorithm that performs simulated annealing method."""
 
-from typing import Callable, Union, Any, Dict, List, Tuple
-from enum import Enum
-from numpy import int32, int64, float32, float64, min, max, array
+from typing import Callable, Any, Dict, List, Tuple
+from numpy import int32, int64, float32, float64, min, max
 from numpy.random import random
 from matplotlib.pyplot import subplots, show
-
-RealNumber = Union[int, float, int32, int64, float32, float64]
-PositiveNumber = Union[int, float, int32, int64, float32, float64]
-Scope = Dict[str, List[Union[int, RealNumber, PositiveNumber, array]]]
+from DataTypes import ScopeParams, Scope, PositiveNumber, RealNumber, NonNegativeInt
 
 
-class ScopeParams(Enum):
-    iteration: str = 'iteration'
-    temperature: str = 'temperature'
-    delta_energy: str = "delta_energy"
-    probability_of_transition: str = "prob_of_trans"
-    cost_function: str = 'cost'
-    solution: str = 'solution'
+class SimulatedAnnealingValidationError(Exception):
+    def __init__(self, message, value=None):
+        self.value = value
+        self.message = message
+        super().__init__(self.message)
 
 
 def plot_scope(scope: Scope) -> None:
@@ -31,30 +25,30 @@ def plot_scope(scope: Scope) -> None:
 
     figure, axes = subplots(2, 2, figsize=(10, 10))
 
-    axes[0, 0].plot(scope[ScopeParams.iteration], scope[ScopeParams.temperature])
-    axes[0, 0].set_title(ScopeParams.temperature.value)
+    axes[0, 0].plot(scope.iteration, scope.temperature)
+    axes[0, 0].set_title(ScopeParams.iteration.value)
     axes[0, 0].set_xlabel(ScopeParams.iteration.value)
     axes[0, 0].set_ylabel(ScopeParams.temperature.value)
     axes[0, 0].grid(True)
 
-    axes[1, 0].plot(scope[ScopeParams.iteration], scope[ScopeParams.delta_energy], 'b.', markersize=1)
+    axes[1, 0].plot(scope.iteration, scope.delta_energy, 'b.', markersize=1)
     axes[1, 0].set_title(ScopeParams.delta_energy.value)
     axes[1, 0].set_xlabel(ScopeParams.iteration.value)
     axes[1, 0].set_ylabel(ScopeParams.delta_energy.value)
     axes[1, 0].grid(True)
 
-    axes[1, 1].plot(scope[ScopeParams.iteration], scope[ScopeParams.probability_of_transition], 'b.', markersize=1)
+    axes[1, 1].plot(scope.iteration, scope.probability_of_transition, 'b.', markersize=1)
     axes[1, 1].set_title(ScopeParams.probability_of_transition.value)
     axes[1, 1].set_xlabel(ScopeParams.iteration.value)
     axes[1, 1].set_ylabel(ScopeParams.probability_of_transition.value)
     axes[1, 1].grid(True)
 
-    axes[0, 1].plot(scope[ScopeParams.iteration], scope[ScopeParams.cost_function])
+    axes[0, 1].plot(scope.iteration, scope.cost_function)
     axes[0, 1].set_title(ScopeParams.cost_function.value)
     axes[0, 1].set_xlabel(ScopeParams.iteration.value)
     axes[0, 1].set_ylabel(ScopeParams.cost_function.value)
     axes[0, 1].grid(True)
-    axes[0, 1].set_ylim([min([0, min(scope[ScopeParams.cost_function])]), max(scope[ScopeParams.cost_function]) + 0.1])
+    axes[0, 1].set_ylim([min([0, min(scope.cost_function)]), max(scope.cost_function) + 0.1])
 
     show()
 
@@ -66,16 +60,16 @@ def generate_sa_algorithm(Solution: Any) -> Callable:
                             init_temp: PositiveNumber,
                             cool: Callable[[PositiveNumber, int], PositiveNumber],
                             probability: Callable[[RealNumber, PositiveNumber], RealNumber],
-                            max_iterations: int = 1000) -> Tuple[Solution, Scope]:
+                            max_iterations: NonNegativeInt = 1000) -> Tuple[Solution, Scope]:
         """
         Function takes all the arguments that simulated annealing method requires and returns the best possible
-        minimal solution.
+        minimal visited_solution.
 
         :param cost: Objective function that is being optimized
         :type cost: Callable returning real value
-        :param init_sol: Starting solution for the algorithm
+        :param init_sol: Starting visited_solution for the algorithm
         :type init_sol: Solution
-        :param sol_gen: Function that returns next solution, randomly different from provided
+        :param sol_gen: Function that returns next visited_solution, randomly different from provided
         :type sol_gen: Solution
         :param init_temp: Starting temperature
         :type init_temp: Real value
@@ -85,17 +79,36 @@ def generate_sa_algorithm(Solution: Any) -> Callable:
         :type probability: Callable returning real value between 0 and 1
         :param max_iterations: Max iterations of the main loop
         :type max_iterations: int
-        :return: Most optimal solution that has been found during search, runtime simulation values
+        :return: Most optimal visited_solution that has been found during search, runtime simulation values
         :rtype: Solution, Scope
         """
 
-        simulation_scope: Scope = {
+        if not callable(cost):
+            raise SimulatedAnnealingValidationError(message="cost is not callable")
+
+        if not callable(sol_gen):
+            raise SimulatedAnnealingValidationError(message="sol_gen is not callable")
+
+        if not isinstance(init_temp, (int, float, int32, int64, float32, float64)):
+            raise SimulatedAnnealingValidationError(message="init_temp must be int or float or int32 or int64 or "
+                                                            "float32 or float64")
+
+        if not callable(cool):
+            raise SimulatedAnnealingValidationError(message="cool is not callable")
+
+        if not callable(probability):
+            raise SimulatedAnnealingValidationError(message="probability is not callable")
+
+        if not isinstance(max_iterations, (int, int32, int64)):
+            raise SimulatedAnnealingValidationError(message='max_iterations must be int', value=max_iterations)
+
+        simulation_scope_data: Dict[ScopeParams.name: List] = {
             ScopeParams.iteration: list(),
             ScopeParams.temperature: list(),
             ScopeParams.delta_energy: list(),
             ScopeParams.probability_of_transition: list(),
             ScopeParams.cost_function: list(),
-            ScopeParams.solution: list()
+            ScopeParams.visited_solution: list()
         }
 
         def update_scope(scope, i, temp, delta_en, prob, cost_val, sol):
@@ -104,7 +117,7 @@ def generate_sa_algorithm(Solution: Any) -> Callable:
             scope[ScopeParams.delta_energy].append(delta_en)
             scope[ScopeParams.probability_of_transition].append(prob)
             scope[ScopeParams.cost_function].append(cost_val)
-            scope[ScopeParams.solution].append(sol)
+            scope[ScopeParams.visited_solution].append(sol)
 
         solution: Solution = init_sol
         temperature: PositiveNumber = init_temp
@@ -124,7 +137,7 @@ def generate_sa_algorithm(Solution: Any) -> Callable:
                 if random(1) < prob_of_transition:
                     solution: Solution = neighbour
 
-            update_scope(scope=simulation_scope,
+            update_scope(scope=simulation_scope_data,
                          i=it,
                          temp=temperature,
                          delta_en=delta_energy,
@@ -133,6 +146,17 @@ def generate_sa_algorithm(Solution: Any) -> Callable:
                          sol=solution)
 
             temperature: PositiveNumber = cool(temperature, it)
+
+        # create Scope only after the algorithm has ended, validations take too long if reassignments were to be
+        # performed within the main loop
+        simulation_scope = Scope()
+
+        simulation_scope.iteration = simulation_scope_data[ScopeParams.iteration]
+        simulation_scope.temperature = simulation_scope_data[ScopeParams.temperature]
+        simulation_scope.delta_energy = simulation_scope_data[ScopeParams.delta_energy]
+        simulation_scope.probability_of_transition = simulation_scope_data[ScopeParams.probability_of_transition]
+        simulation_scope.cost_function = simulation_scope_data[ScopeParams.cost_function]
+        simulation_scope.visited_solution = simulation_scope_data[ScopeParams.visited_solution]
 
         return solution, simulation_scope
 
@@ -148,7 +172,7 @@ def main():
     def cost_fun(vec):
         (x, y) = vec
 
-        return x**2 + y**2
+        return x ** 2 + y ** 2
 
     def sol_gen_fun(vec):
         (x, y) = vec
@@ -179,7 +203,7 @@ def main():
     def colling_fun(_, k):
         temp = (1 - (k + 1) / max_iter) * start_temp
 
-        if temp <= 10**(-9):
+        if temp <= 10 ** (-9):
             return 0.0001
         return temp
 
@@ -191,7 +215,7 @@ def main():
     solution_type = Tuple[Union[int, float], Union[int, float]]
     sim_an = generate_sa_algorithm(solution_type)
 
-    max_iter = 5000000
+    max_iter = 10**3
     start_temp = 0.1
     init_sol_tup = (2, -2)
 
@@ -206,11 +230,12 @@ def main():
     plot_scope(scope)
     print(sol)
 
-    x_vec = [tup[0] for tup in scope[ScopeParams.solution]]
-    y_vec = [tup[1] for tup in scope[ScopeParams.solution]]
+    x_vec = [tup[0] for tup in scope.visited_solution]
+    y_vec = [tup[1] for tup in scope.visited_solution]
 
     plt.plot(x_vec, y_vec)
     plt.plot(0, 0, 'r.')
+    plt.plot(sol[0], sol[1], 'g.')
     plt.title('solutions')
     plt.show()
 
