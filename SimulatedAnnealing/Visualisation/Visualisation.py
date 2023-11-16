11d249d039a5fd2965baf9fa1a4df44881b008b2
koltonjakub <102661671+koltonjakub@users.noreply.github.com>
2023-11-16 09:22:22 +0100

    ScopeParams deleted, Scope.label dict now serves as enum type for string labels.

--- SimulatedAnnealing/Visualisation/Visualisation.py
+++ SimulatedAnnealing/Visualisation/Visualisation.py
@@ -4,8 +4,7 @@
 from matplotlib.pyplot import subplots, show
 from numpy import min, max
 from pydantic import BaseModel, BaseConfig, Field, conint, confloat
-from typing import List, Any, Annotated
-from enum import Enum
+from typing import List, Any, Dict
 
 
 class UnvalidatedScope(BaseModel):
@@ -17,18 +16,22 @@ class UnvalidatedScope(BaseModel):
         validate_assignment = False  # Allows the model to validate data every time field is assigned/changed
         smart_union = True  # Prevents unnecessary casts to not matching data types
 
-    # TODO add best solution encountered cost function field, chatGPT suggests:
-    # 1: Class Variables
-    # 2: extra field: Dict[str: str] - maps attribute name to its label
-    # 3: Custom Metaclass or Decorator(probably overkill)
-
     iteration: List[conint(ge=0, strict=True)] = []
     temperature: List[confloat(ge=0)] = []
-    delta_energy: List[confloat()] = []  # probably useless
+    delta_energy: List[confloat()] = []
     probability_of_transition: List[confloat(ge=0, le=1)] = []
     cost_function: List[confloat(ge=0)] = []
     best_cost_function: List[confloat(ge=0)] = []
     visited_solution: List[Any] = []
+    label: Dict[str, str] = Field({
+        'iteration': 'encounter',
+        'temperature': 'temperature',
+        'delta_energy': "delta_energy",
+        'probability_of_transition': "prob_of_trans",
+        'cost_function': 'objective_value',
+        'best_cost_function': 'best_cost_function',
+        'visited_solution': 'visited_solution'
+    }, frozen=True)
 
     # TODO consider writing custom @validator that checks only one value rather than whole list when new
     # TODO value is assigned
@@ -42,18 +45,6 @@ class ValidatedScope(UnvalidatedScope):
         smart_union = True  # Prevents unnecessary casts to not matching data types
 
 
-class ScopeParams(Enum):
-    """Enum type of all data available within UnvalidatedScope type"""
-
-    iteration: str = 'encounter'
-    temperature: str = 'temperature'
-    delta_energy: str = "delta_energy"
-    probability_of_transition: str = "prob_of_trans"
-    cost_function: str = 'objective_value'
-    best_cost_function: str = 'best_cost_function'
-    visited_solution: str = 'visited_solution'
-
-
 def plot_scope(scope: UnvalidatedScope) -> None:
     """
     Function that plots runtime values of simulation from Scope type.
@@ -64,28 +55,28 @@ def plot_scope(scope: UnvalidatedScope) -> None:
     figure, axes = subplots(2, 2, figsize=(10, 10))
 
     axes[0, 0].plot(scope.iteration, scope.temperature)
-    axes[0, 0].set_title(ScopeParams.temperature.value)
-    axes[0, 0].set_xlabel(ScopeParams.iteration.value)
-    axes[0, 0].set_ylabel(ScopeParams.temperature.value)
+    axes[0, 0].set_title(scope.label['temperature'])
+    axes[0, 0].set_xlabel(scope.label['iteration'])
+    axes[0, 0].set_ylabel(scope.label['temperature'])
     axes[0, 0].grid(True)
 
     axes[0, 1].plot(scope.iteration, scope.probability_of_transition, 'b.', markersize=1)
-    axes[0, 1].set_title(ScopeParams.probability_of_transition.value)
-    axes[0, 1].set_xlabel(ScopeParams.iteration.value)
-    axes[0, 1].set_ylabel(ScopeParams.probability_of_transition.value)
+    axes[0, 1].set_title(scope.label['probability_of_transition'])
+    axes[0, 1].set_xlabel(scope.label['iteration'])
+    axes[0, 1].set_ylabel(scope.label['probability_of_transition'])
     axes[0, 1].grid(True)
 
     axes[1, 0].plot(scope.iteration, scope.best_cost_function)
-    axes[1, 0].set_title(ScopeParams.best_cost_function.value)
-    axes[1, 0].set_xlabel(ScopeParams.iteration.value)
-    axes[1, 0].set_ylabel(ScopeParams.best_cost_function.value)
+    axes[1, 0].set_title(scope.label['best_cost_function'])
+    axes[1, 0].set_xlabel(scope.label['iteration'])
+    axes[1, 0].set_ylabel(scope.label['best_cost_function'])
     axes[1, 0].grid(True)
     axes[1, 0].set_ylim([min([0, min(scope.best_cost_function)]), max(scope.best_cost_function) + 0.1])
 
     axes[1, 1].plot(scope.iteration, scope.cost_function)
-    axes[1, 1].set_title(ScopeParams.cost_function.value)
-    axes[1, 1].set_xlabel(ScopeParams.iteration.value)
-    axes[1, 1].set_ylabel(ScopeParams.cost_function.value)
+    axes[1, 1].set_title(scope.label['cost_function'])
+    axes[1, 1].set_xlabel(scope.label['iteration'])
+    axes[1, 1].set_ylabel(scope.label['cost_function'])
     axes[1, 1].grid(True)
     axes[1, 1].set_ylim([min([0, min(scope.cost_function)]), max(scope.cost_function) + 0.1])
 
@@ -94,12 +85,10 @@ def plot_scope(scope: UnvalidatedScope) -> None:
 
 if __name__ == "__main__":
     def scope_usage_example():
-        scp = UnvalidatedScope()
-        scp.iteration = [1, 0, 2.0]  # Type coercion is always ON, probably due to
+        scp = ValidatedScope()
+        scp.iteration = [1, 0]  # Type coercion is always ON, probably due to
         scp.iteration += [1]  # The only proper way to append to list, .append() method omits the pydantic validation
 
-        scp.probability_of_transition = [1.2]
-        scp.probability_of_transition += [1.2]
         scp.delta_energy = [-1]
         scp.delta_energy += [-1]
 
