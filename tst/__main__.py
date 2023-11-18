b6f4971802e26e05f8d5c52629f0f8196a54a7f4
koltonjakub <102661671+koltonjakub@users.noreply.github.com>
2023-11-18 21:08:10 +0100

    ScopeTests.py added to tst/__main__.py. All 57 pass.

--- tst/__main__.py
+++ tst/__main__.py
@@ -1,19 +1,21 @@
 import unittest
+
+from SolverTests import SolverTests
+from VisualisationTests import ScopeTests
 from FactoryAssignmentProblemTests import (MachineTests, EmployeeTests, TimeSpanTests,
                                            ResourceContainerTests, ResourceManagerTests, FactoryAssignmentScheduleTests)
-from SolverTests import SolverTests
 
 if __name__ == '__main__':
     test_suite = unittest.TestSuite()
 
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(SolverTests))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ScopeTests))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(MachineTests))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(EmployeeTests))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TimeSpanTests))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ResourceContainerTests))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ResourceManagerTests))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(FactoryAssignmentScheduleTests))
-
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(SolverTests))
 
     test_result = unittest.TestResult()
     test_suite.run(result=test_result)
